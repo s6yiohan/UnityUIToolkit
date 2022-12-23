6ac00e849d08a2323e84521dc7b39aad2c59a1b9
Yiohan Catubag <aerol.catubag@secret6.com>
2022-12-23 17:09:42 +0800

    Main menu initial setup

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
+
+    public Button startButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        startButton = root.Q<Button>("start-button");
     }
 
     // Update is called once per frame
